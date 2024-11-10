1266aa9e1e5935f269b2e0b7cdfcbad5bdbd562f
KeerthikaReddyChalla <keerthikareddy2005@gmail.com>
2024-11-10 14:57:23 +0530

    dropdown added

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 // Function to show the corresponding section when a link is clicked
 function showSection(sectionId) {
-    // Hide all sections
-    const sections = document.querySelectorAll('.content-section');
-    sections.forEach(section => {
-      section.classList.remove('active');
-    });
-  
-    // Show the clicked section
-    const sectionToShow = document.getElementById(sectionId);
-    if (sectionToShow) {
-      sectionToShow.classList.add('active');
-    }
-  }
-  
-  // Optionally, you can show the 'home' section by default when the page loads
-  document.addEventListener("DOMContentLoaded", function() {
-    showSection('home');
+  // Hide all sections
+  const sections = document.querySelectorAll('.content-section');
+  sections.forEach(section => {
+    section.classList.remove('active');
   });
-  
+
+  // Show the clicked section
+  const sectionToShow = document.getElementById(sectionId);
+  if (sectionToShow) {
+    sectionToShow.classList.add('active');
+  }
+}
+
+// Show the 'home' section by default when the page loads
+document.addEventListener("DOMContentLoaded", function() {
+  showSection('home');
+});
