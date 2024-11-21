4cffd187a2ada6a92c612b541dc0fe2a109eedc1
shravani2905 <shravanireddypothula@gmail.com>
2024-11-21 11:47:04 +0530

    commit done

--- script.js
+++ script.js
@@ -17,30 +17,3 @@ function showSection(sectionId) {
 }
 
 
-
-
-// Initialize Swiper
-var swiper = new Swiper('.swiper-container', {
-  loop: true, // Enable looping
-  autoplay: {
-    delay: 2500, // Delay between slides
-    disableOnInteraction: false,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  breakpoints: {
-    768: {
-      slidesPerView: 1, // Show one slide at a time on smaller screens
-    },
-    576: {
-      slidesPerView: 1, // Show one slide at a time on mobile
-    },
-  },
-});
-
